51cbcd447ee70d3398696a78f48daf3754a105c9
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-10-29 21:20:16 -0400

    Added frus_volume_id column to mosaic.docs view

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 create schema mosaic;
-create view mosaic.docs as select * from foiarchive.docs;
+create or replace view mosaic.docs (
+       doc_id, corpus, classification, authored, title, 
+       body, full_text, source, frus_volume_id) 
+as 
+select d.doc_id, d.corpus, d.classification, d.authored, d.title, 
+       d.body, d.full_text, d.source, f.volume_id
+    from foiarchive.docs d left join foiarchive.docs_frus f 
+                                        on d.doc_id = f.doc_id;
 
 create role mosaic_team_member;
 grant usage on schema mosaic to mosaic_team_member;
