ef8607a89729df8b5c7384c9ec718e7c7eaa6f69
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-11-14 08:30:39 -0500

    Changed reference from source to doc_url

--- oct73.sql
+++ oct73.sql
@@ -1,7 +1,7 @@
 create or replace view mosaic.oct73 as 
-select doc_id, corpus, classification, authored, 
-       coalesce(source, 'http://history-lab.org/documents/' || doc_id) source, 
+select doc_id, corpus, classification, authored, doc_url, 
        substring(body, 1, 1024) body_1k, 
        frus_volume_id
     from mosaic.docs
-    where authored >= '1973-10-01' and authored < '1973-11-01';
+    where authored >= '1973-10-01' and authored < '1973-11-01'
+    order by authored;
