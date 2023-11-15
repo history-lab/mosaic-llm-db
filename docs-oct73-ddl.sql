0ba29f41a3e0ae8fb3e9af62af5de46b6b1cf968
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-11-15 08:44:49 -0500

    Initial commit

---
+++ docs-oct73-ddl.sql
@@ -0,0 +1,13 @@
+create table mosaic.docs_oct73 (
+    doc_id              varchar(40) primary key,
+    corpus              varchar(10) not null /* add reference */,
+    title               text        not null,
+    doc_url             text        not null, 
+    classification      varchar(20) /* add reference */,
+    authored            timestamp with time zone,
+    frus_volume_id      text,
+    body                text        not null,
+    pg_cnt              integer,
+    word_cnt            integer     /* not null */,
+    fts                 tsvector    /* not null */
+);
