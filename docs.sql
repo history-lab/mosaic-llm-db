7549b37d0809096405f1dce1435577561c1f4aa0
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-02-06 11:44:10 -0500

    Streamlined definition of mosaic.docs

--- docs.sql
+++ docs.sql
@@ -1,23 +1,16 @@
-create schema mosaic;
+drop view if exists mosaic.docs;
 create or replace view mosaic.docs (
        doc_id, corpus, classification, authored, title, 
-       body, full_text, doc_url, frus_volume_id) 
+       body, full_text, doc_url) 
 as 
 select d.doc_id, d.corpus::text, d.classification::text, d.authored, d.title, 
        d.body, d.full_text, 
-       coalesce(d.source, 'http://history-lab.org/documents/' || d.doc_id) doc_url, 
-       f.volume_id
-    from foiarchive.docs d left join foiarchive.docs_frus f 
-                                        on d.doc_id = f.doc_id
-    where /* exclude no body documents from CFPF */
-          d.body != '' and
-          d.body != 'No Text Body Available for Document' and
-          d.body not like 'MRN: %TEXT FOR THIS MRN IS UNAVAILABLE%'
+       coalesce(d.source, 'http://history-lab.org/documents/' || d.doc_id) doc_url
+    from foiarchive.docs d 
 union all
-select g.id::id_d doc_id, 'usddo' corpus, lower(g.classification) classification,
+select g.id::foiarchive.id_d doc_id, 'usddo' corpus, lower(g.classification) classification,
        g.date authored, g.title, g.body, NULL::tsvector full_text,
-       'http://history-lab.org/documents/' || g.id doc_url, 
-       NULL::id_d frus_volume_id
+       'http://history-lab.org/documents/' || g.id doc_url
    from declassification_ddrs.docs g;
 
 
